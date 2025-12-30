443fff40395bba8ca442b41f8e5a1ef2a1536d0c
RoyRu26 <roy.rutzky@gmail.com>
2025-12-30 11:46:59 +0200

    intobin completed

--- IntToBin.java
+++ IntToBin.java
@@ -5,7 +5,6 @@ public class IntToBin {
         int x = Integer.parseInt(args[0]);
         System.out.println("Binary representation of " + x + " is: " + toBinary(x));
     }
-
     /**
      * Returns the binary representation of the given integer, as a string of 0's
      * and 1's.
@@ -14,7 +13,9 @@ public class IntToBin {
         if(x == 0){
             return "0";
         }
-        String result = toBinary(x / 2) + (x % 2);
-        return result;
+        if(x == 1){
+            return "1";
+        }
+        return toBinary(x / 2) + (x % 2);
     }
 }
