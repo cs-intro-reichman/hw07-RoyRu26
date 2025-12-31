641b4c2ae702362769b28ed6e13f8d74d051b8ee
RoyRu26 <roy.rutzky@gmail.com>
2025-12-31 15:14:49 +0200

    check sierpinski

--- Koch.java
+++ Koch.java
@@ -8,14 +8,14 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		
 
 		/*
 		// Tests the snowflake function:
@@ -27,8 +27,28 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
+		if (n == 0) {
+			return;
+		}
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		double x13 = (x1 + x2) / 3;
+		double x23 = (x1 + x2) / 3 + x13;
+		double p3x = (y1 - y2) * (Math.sqrt(3) / 6) + (0.5 * (x1 + x2));
+		double p3y = (x2 - x1) * (Math.sqrt(3) / 6) + (0.5 * (y1 + y2));
+		System.out.println(x2 - x1);
+		
+		//erases the middle of the line
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.line(x13, y1, x23, y2);
+
+		//draws the triangle sides
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(x13, y1, p3x, p3y);
+		StdDraw.line(p3x, p3y, x23, y2);
+
+		//recursion call
+		curve(n - 1, x1, y1, x13, y2);
+		//curve(n - 1, x23, y1, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
--- Sierpinski.java
+++ Sierpinski.java
@@ -1,18 +1,57 @@
+
 /** Draws ths Sierpinski Triangle fractal. */
+
 public class Sierpinski {
-	
+
 	public static void main(String[] args) {
 		sierpinski(Integer.parseInt(args[0]));
 	}
-	
-	// Draws a Sierpinski triangle of depth n on the standard canvass.
-	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+
+	public static void sierpinski(int n) {
+		StdDraw.clear();
+		StdDraw.setPenColor(StdDraw.BLACK);
+
+		double s = 0.9;
+		double h = s * Math.sqrt(3) / 2;
+		double m = 0.05;
+
+		double x1 = 0.5 - s / 2;
+		double y1 = m;
+
+		double x2 = 0.5 + s / 2;
+		double y2 = m;
+
+		double x3 = 0.5;
+		double y3 = m + h;
+
+		StdDraw.line(x1, y1, x2, y2);
+		StdDraw.line(x2, y2, x3, y3);
+		StdDraw.line(x3, y3, x1, y1);
+
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
 	}
-	
+
 	// Does the actual drawing, recursively.
-	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+	private static void sierpinski(int n, double x1, double x2, double x3, double y1, double y2, double y3) {
+		if (n == 0) {
+			return;
+		}
+
+		double x12 = (x1 + x2) / 2.0;
+		double y12 = (y1 + y2) / 2.0;
+
+		double x23 = (x2 + x3) / 2.0;
+		double y23 = (y2 + y3) / 2.0;
+
+		double x13 = (x1 + x3) / 2.0;
+		double y13 = (y1 + y3) / 2.0;
+
+		StdDraw.line(x12, y12, x23, y23);
+		StdDraw.line(x23, y23, x13, y13);
+		StdDraw.line(x13, y13, x12, y12);
+
+		sierpinski(n - 1, x1, x12, x13, y1, y12, y13);
+		sierpinski(n - 1, x12, x2, x23, y12, y2, y23);
+		sierpinski(n - 1, x13, x23, x3, y13, y23, y3);
 	}
 }
